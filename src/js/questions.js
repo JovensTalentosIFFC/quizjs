d5bd76ad438d4fc42ed501d638a1cc25a594059a
Rian <infosisfaperj@gmail.com>
2025-08-04 11:40:41 -0300

    feat: mostrar correta e incorreta

--- src/js/questions.js
+++ src/js/questions.js
@@ -2,10 +2,13 @@ const numberAnswer = document.querySelector('aside .perguntaContainer .pergunta 
 const answer = document.querySelector('aside .perguntaContainer .pergunta p')
 
 const options = Array.from(document.querySelectorAll('.respostas form .labels label'));
-const currentLevel = localStorage.getItem('currentLevel') || 1;
-const currentQuestionIndex = localStorage.getItem('currentQuestion') || 1;
+let currentLevel = localStorage.getItem('currentLevel') || 1;
+let currentQuestionIndex = localStorage.getItem('currentQuestion') || 1;
 let setupQuestion = localStorage.getItem('setupQuestion') || '';
 const form = document.querySelector('form');
+
+let lifes, clues;
+
 (async () => {
 
   
@@ -63,9 +66,38 @@ const form = document.querySelector('form');
     })
   }
   localStorage.setItem('setupQuestion', setupQuestion);
-  console.log(seenOption)
 
+ 
   
+  form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  console.log('submt')
+
+  const selectedOptIndex = options.findIndex(opt => Array.from(opt.classList).includes('selected')) || 0
+  const selectedOpt = options[selectedOptIndex];
+  if(!selectedOptIndex){
+    // show message asking for select one question
+
+    return;
+  }
+  form.style.pointerEvents='none';
+  let correctIndex = Array.from(setupQuestion).findIndex(opt => opt===currentQuestion.correct);
+  if(selectedOptIndex !== correctIndex){
+    selectedOpt.classList.add('missed');
+    options[correctIndex].classList.add('selected');
+  } else{
+    // correct answer;
+  }
+  if(currentQuestionIndex===5){
+    currentLevel++;
+    currentQuestionIndex=0;
+  }
+  currentQuestionIndex++;
+  localStorage.setItem('currentQuestion', currentQuestionIndex);
+  localStorage.setItem('currentLevel', currentLevel);
+
+})
+
 })()
 
 function selectOption(selectedOpt){
@@ -76,12 +108,3 @@ function selectOption(selectedOpt){
     }
   })
 }
-form.addEventListener('submit', (e) =>{
-  e.preventDefault();
-  console.log('submt')
-
-  const selectedOpt = options.filter(opt => Array.from(opt.classList).includes('selected'))[0].textContent || 0
-  if(!selectedOpt){
-    // style to show messages
-  }
-})
