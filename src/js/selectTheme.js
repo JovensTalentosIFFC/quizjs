d2cab3adbae5983d6654a78320d64889b8bdd2e9
Rian <infosisfaperj@gmail.com>
2025-09-01 17:13:19 -0300

    fix: set theme on redirect

--- src/js/selectTheme.js
+++ src/js/selectTheme.js
@@ -1,5 +1,6 @@
 const redirectToBattle = (e) =>{
   console.log(e.textContent);
   localStorage.setItem('theme', e.textContent);
+  localStorage.setItem('currentTheme', e.textContent)
   window.location = 'battle.html'
 }
